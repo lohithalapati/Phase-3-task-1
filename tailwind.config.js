83e5af1cb946dd940e294befb6883b86a5e2428e
lohithalapati <youremail@gmail.com>
2026-07-12 20:04:29 +0530

    chore: track foundational build and styling configuration files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontSize: {
+        '2xs': '0.65rem'
+      }
+    },
+  },
+  plugins: [],
+}
